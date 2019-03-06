8e3927c831dfb9994c22664a8fb30c488461ba11
Tech-a53 <tech-a53@Tech-A53s-MacBook-Air.local>
2019-03-06 10:24:41 +0000

    added local host

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,7 +11,7 @@ Capybara.configure do |config|
   config.default_max_wait_time= 10 #wait time for asynchronus processes to finsh
   config.match = :prefer_exact #this setting is to ensure Capybara has specific matching rather than fuzzy logic
   config.default_driver = :chrome # ensures chrome is the default driver
-  config.app_host = '<url here>' # provides the app host/core url
+  config.app_host = 'http://localhost:9292/' # provides the app host/core url
 end
 
 
