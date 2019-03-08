313e9fc87f375138117413bda35f8d8ec88dc77a
Tech-a51 <tech-a51@TECH-A51s-MacBook-Air.local>
2019-03-08 09:26:52 +0000

    spec1 to spec 4 done

--- features/step_definitions/specialization_step_defs.rb
+++ features/step_definitions/specialization_step_defs.rb
@@ -1,53 +1,53 @@
 When("I click the Specialization link") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.click_specialization_link
 end
 
 Then("the page displaying a list of all Specializations is open") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(current_url).to eq 'http://localhost:9292/specialisations'
+  expect(specialization.list_table_header).to eq 'Specialisation Name'
+  specialization.check_list_table
 end
 
-When("the page displaying a list of all Specializations is open") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
 
 When("I click a specialization") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.click_specialization 'SDET'
 end
 
 Then("I arrive at the individual specialization show page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(current_url).to eq 'http://localhost:9292/specialisations/1'
 end
 
 When("I click the add new Specialization link") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.add_specialization_link
+  expect(current_url).to eq 'http://localhost:9292/specialisations/new'
 end
 
 When("I enter a new Specialization") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.enter_specialization 'test3'
+  sleep 3
 end
 
 When("click the save click") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.click_save
+  sleep 3
 end
 
 Then("I should be on the Specialization page with the new Specialization added") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(current_url).to eq 'http://localhost:9292/specialisations'
+  specialization.check_new_specialization_added 'test3'
+  sleep 3
 end
 
 Then("I should receive an error about the specialization having no name") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click an specialization") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(specialization.error_message).to eq 'Please enter a value'
 end
 
 When("I click the edit link") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.click_edit
 end
 
 When("I edit the Specialization") do
-  pending # Write code here that turns the phrase above into concrete actions
+  specialization.enter_specialization 'SDET'
 end
 
 Then("I should be on the Specialization page with the edited Specialization updated") do
